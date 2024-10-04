e7b9a5feb5d0e6b9c4794a8754701806eb6ed836
silpakammara99 <silpakammara9702@gmail.com>
2024-10-04 16:09:35 +0530

    conditional rendering

---
+++ src/componets creation/conditionalRendering/conditionalRendering.jsx
@@ -0,0 +1,14 @@
+
+
+const ConditionalRendering=()=>{
+   
+
+    if(true){
+        return <h1>hello Conditional Rendering</h1>
+    }
+    else{
+        return <h1>Hello non-conditional Rendering</h1>
+    }
+    
+}
+export default ConditionalRendering
